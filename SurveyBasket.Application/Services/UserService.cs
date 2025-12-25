f9e9dc980f7d1fa0e99fbc96793c9a8a304fcf41
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-12-25 14:13:39 +0200

    Add user password change feature with validation
    Implemented ChangePasswordAsync in IUserService and UserService using ASP.NET Identity. Added ChangePasswordRequest DTO and ChangePasswordRequestValidator to enforce password complexity and prevent reuse of the current password.

--- SurveyBasket.Application/Services/UserService.cs
+++ SurveyBasket.Application/Services/UserService.cs
@@ -26,5 +26,20 @@ namespace SurveyBasket.Application.Services
 
             return Result.Success();
         }
+
+        public async Task<Result> ChangePasswordAsync(string userId, ChangePasswordRequest request)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var result = await _userManager.ChangePasswordAsync(user!, request.CurrentPassword, request.NewPassword);
+
+            if (result.Succeeded)
+                return Result.Success();
+
+            var error = result.Errors.First();
+
+            return Result.Failure(new Error(error.Code, error.Description, StatusCodes.Status400BadRequest));
+
+        }
     }
 }
---
+++ SurveyBasket.Application/Validations/Users/ChangePasswordRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using SurveyBasket.Application.Abstractions.Abstractions.Const;
+using SurveyBasket.Application.Abstractions.DTOs.Users.Request;
+
+namespace SurveyBasket.Application.Validations.Users
+{
+    public class ChangePasswordRequestValidator :AbstractValidator<ChangePasswordRequest>
+    {
+        public ChangePasswordRequestValidator()
+        {
+            RuleFor(p => p.CurrentPassword)
+                .NotEmpty()
+                .WithMessage("");
+
+            RuleFor(p => p.NewPassword)
+                .NotEmpty()
+                .Matches(RegexPatterns.Password)
+                .WithMessage("Password must be at least 8 digits  and contain at least one uppercase letter, one lowercase letter, one digit and one special character ")
+                .NotEqual(p=>p.CurrentPassword)
+                .WithMessage("New Password cannot be same as the current password");
+
+        }
+    }
+}
